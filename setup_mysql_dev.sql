91e53132d4a9632130ff988880e98496b07de361
Masonga222 <masongashemap@gmail.com>
2024-02-17 08:21:08 -0800

    AirBnB_clone_v2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,6 @@
 -- Creates database hbnb_dev_db, user hbnb_dev and grants priveleges
 
-HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db ./console.py
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
