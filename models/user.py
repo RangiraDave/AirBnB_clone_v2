921a7dd11f2150a140c1111fff6a2c95e9803202
RangiraDave <rangiradev666@gmail.com>
2024-02-19 13:47:40 +0200

    Updated User class

--- models/user.py
+++ models/user.py
@@ -2,11 +2,11 @@
 """
 This module defines a class User
 """
-from models.base_model import BaseModel, Base
+from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 
 
-class User(BaseModel, Base):
+class User(Base, BaseModel):
     """
     This class defines a user by various attributes
     """
