7ef3ca7a0fccca49470cb22b114db5222314a149
RangiraDave <rangiradev666@gmail.com>
2024-02-20 08:30:11 +0200

    Added execution permissions to all

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module for testing file storage
+"""
+
+import unittest
+from models.base_model import BaseModel, Base
+from models import storage
+import os
+
+
+class test_fileStorage(unittest.TestCase):
+    """ Class to test the db storage method """
+    pass
