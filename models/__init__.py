9a58bb6a84b4f5544c63ea8120ce8b6056f31c77
Masonga222 <masongashemap@gmail.com>
2024-02-24 00:35:10 -0800

    AirBnB_clone_v2

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-    """This module instantiates an object of class FileStorage"""
-
+'''
+    Package initializer
+'''
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
