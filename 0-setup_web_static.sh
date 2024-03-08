bf3f0aec4603543776d7013f867724eb5003bb0e
RangiraDave <rangiradev666@gmail.com>
2024-03-08 10:36:21 +0200

    changed logic

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,42 +1,17 @@
 #!/usr/bin/env bash
-# Bash script to setup my servers ready for the
-# Deployment of web_static
-
-# nginx installation
-sudo apt-get -y update
-sudo apt-get -y install nginx
-
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-
-sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
-<html>
-	<head></head>
-	<body>
-		Fake content to test NGINX configuration
-	</body>
-</html>
-EOF
-
-# Creating symbolic link
-sudo rm -f /data/web_static/current
-sudo ln -s /data/web_static/releases/test /data/web_static/current
-
-# changing ownership to ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
-
-NGINX_CONFIG='/etc/nginx/nginx.conf'
-NGINX_CONFIG_BLOCK='
-	server {
-		location /hbnb_static {
-			alias /data/web_static/current/;
-		}
-	}
-'
-
-# Checking if content exsists in the config file
-if ! grep -qF "$NGINX_CONFIG_BLOCK" "$NGINX_CONFIG"; then
-	sudo sed -i '/http {/a '"$NGINX_CONFIG_BLOCK"'' "$NGINX_CONFIG"
-fi
-# Restarting the nginx to apply changes
-sudo service nginx restart
+# Sets up your web servers for the deployment of web_static
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+touch /data/web_static/releases/test/index.html
+echo "Simple content, to test the configuration" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+file='/etc/nginx/sites-available/default'
+line=56
+text='\t}\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}'
+sed -i "${line}s~.*~${text}~" $file
+service nginx restart
+exit 0
