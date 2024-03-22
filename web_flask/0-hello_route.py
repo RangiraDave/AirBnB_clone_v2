53b7499ffae83df69f85e3be854a74e156fa1871
RangiraDave <rangiradev666@gmail.com>
2024-03-22 18:01:31 +0200

    Added firt script

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Script to start a simple Flask web app
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
