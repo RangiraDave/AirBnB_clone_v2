9d6417d81f075c5585c08d61065f077846a96375
RangiraDave <rangiradev666@gmail.com>
2024-03-22 18:39:00 +0200

    Added the function to display HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Python script to start flask web app that returns HBNB
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes=False
+
+@app.route('/')
+def hello():
+    """
+    Function to print the mesage.
+    """
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    Function that displays the simple message.
+    """
+
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
