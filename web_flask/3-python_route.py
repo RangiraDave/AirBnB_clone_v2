2eabdf1acd57a0162092241c20878d6a8c532c84
RangiraDave <rangiradev666@gmail.com>
2024-03-22 20:10:17 +0200

    added python functionality

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Script to add /python/<text> functionality """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """" Desplaying hello hbnb """
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ Desplaying HBNB """
+
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ Desplaying C with the following text """
+
+    text = text.replace('_', ' ') if '_' in text else text
+
+    return f'C {text}'
+
+@app.route('/python/<text>')
+def print_python(text='is cool'):
+    """ Printing python followed by the message """
+
+    text = text.replace('_', ' ') if '_' in text else text
+
+    return f'Python {text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
