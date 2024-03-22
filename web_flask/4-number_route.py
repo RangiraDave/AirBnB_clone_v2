6af577bed48732ea2a16245258f9bb089065bd5f
RangiraDave <rangiradev666@gmail.com>
2024-03-22 20:44:38 +0200

    added int path

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Script to add /python/<text> functionality """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """" Desplaying hello hbnb """
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ Desplaying HBNB """
+
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ Desplaying C with the following text """
+
+    text = text.replace('_', ' ') if '_' in text else text
+
+    return f'C {text}'
+
+
+@app.route('/python')
+@app.route('/python/')
+@app.route('/python/<text>')
+def print(text='is cool'):
+    """ Printing python followed by the message """
+
+    text = text.replace('_', ' ') if '_' in text else text
+
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """ Printing n if its an int """
+
+    return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
