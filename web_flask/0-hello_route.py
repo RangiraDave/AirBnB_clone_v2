e0e43d113bc298975bf97b36867f0310c46bf469
RangiraDave <rangiradev666@gmail.com>
2024-03-22 18:28:01 +0200

    added strict_slashes = False in the route redirection

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,13 +8,12 @@ app = Flask(__name__)
 # app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes = False)
+@app.route("/", strict_slashes = False)
 def hello():
     """
     Function to return the message.
     """
-
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
