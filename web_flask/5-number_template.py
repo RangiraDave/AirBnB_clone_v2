aa93e90359fd135f2969985c51aa58f902bcb625
RangiraDave <rangiradev666@gmail.com>
2024-03-22 21:25:58 +0200

    added the "Number: " in the return value

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -53,7 +53,7 @@ def number(n):
 def number_t(n):
     """ Rendering an HTML page """
 
-    return render_template('5-number.html', n=f"<H1>Number: {n}</H1>")
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
